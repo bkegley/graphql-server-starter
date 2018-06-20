15d4890043a5cfe5e1fdf4dbf79ee45f7b2d010f
Bryan Kegley <bjkegley@gmail.com>
2018-06-20 16:56:36 -0500

    Confrm subscription functionality

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,8 +1,10 @@
 import queries from './queries'
 import mutations from './mutations'
+import { subscription } from './subscriptions'
 
 export default {
     Query: { ...queries },
-    Mutation: { ...mutations }
+    Mutation: { ...mutations },
+    Subscription: { ...subscription}
 }
 
--- resolvers/mutations/cat.js
+++ resolvers/mutations/cat.js
@@ -4,7 +4,7 @@ export const mutation = {
     createCat: async (parent, args, { models }) => {
         const { Cat } = models
         const cat = await new Cat(args).save()
-        pubsub.publish(CAT_ADDED, { cat })
+        pubsub.publish(CAT_ADDED, {'catAdded': cat })
         return cat
     }
 }
