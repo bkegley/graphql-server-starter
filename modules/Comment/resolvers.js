f86e832e5781b438b27dbfc34e8bd34576399d55
Bryan Kegley <bjkegley@gmail.com>
2018-07-06 23:12:26 -0500

    Move interfaces to module resolvers

--- modules/Comment/resolvers.js
+++ modules/Comment/resolvers.js
@@ -26,3 +26,12 @@ export const Comment = {
         return user
     }
 }
+
+export const CommentInterface = {
+    __resolveType: (parent, args, context) => {
+        if (parent.isArticleComment) {
+            return 'ArticleComment'
+        }
+        return 'Comment'
+    }
+}
